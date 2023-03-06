0eba0b8d08ac1064112b8fa7a62a3d2ffdb4cd33
Jakub <hello@skoczen.dev>
2023-03-06 12:52:37 +0100

    config standarization

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/*.test.ts'],
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['**/*.test.ts'],
 }
